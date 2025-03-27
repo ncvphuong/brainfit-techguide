62b85a746ce41fcff85fab503ab77a299c1b9aa9
nguyentanminhchau <nguyentanminhchau@gmail.com>
2025-03-27 07:26:45 +0700

    Remove .docusaurus from repository

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/chaunguyen/Desktop/Power Digital/BF/tech-guide-brainfit/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/chaunguyen/Desktop/Power Digital/BF/tech-guide-brainfit/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/chaunguyen/Desktop/Power Digital/BF/tech-guide-brainfit/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/chaunguyen/Desktop/Power Digital/BF/tech-guide-brainfit/src/css/custom.css"),
-];
